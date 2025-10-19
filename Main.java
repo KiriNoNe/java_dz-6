fa3344f6f4cf31b3ae39db96ee6512a12353e3f6
Новиков Кирилл Васильевич ИП-512 <kirill.novikov.2005.nkv@gmail.com>
2025-10-20 00:46:54 +0700

    Добален метод getCarByBrend

--- Main.java
+++ Main.java
@@ -1,4 +1,37 @@
 public class Main {
     public static void main(String[] args){
+        Car[] cars = new Car[4];
+        cars[0] = new Car(1,"Honda", "ter", 2002, "yellow", 20000.0, 8947534);
+        cars[1] = new Car(2,"BMW", "xx", 2001, "green", 22000.0, 437534);
+        cars[2] = new Car(3,"Lada", "baical", 2000, "yellow", 23000.0, 8657534);
+        cars[3] = new Car(4,"Lada", "seda", 2011, "yellow", 24000.0, 891534);
+        
+        Car[] ladaCars = getCarByBrend(cars, "Lada");
+        System.out.println("Найдено lad: "+ladaCars.length);
+        for (Car item : ladaCars){
+            System.out.println(item.getId());
+            System.out.println(item.getMarka());
+            System.out.println(item.getModel());
+        }
+    }
+    
+    public static Car[] getCarByBrend(Car[] cars, String brend){
+        int count = 0;
+        for (Car car: cars){
+            if(car.getMarka().equalsIgnoreCase(brend)){
+                count++;
+            }
+        }
+
+        Car[] result = new Car[count];
+        int index = 0;
+        
+        for (Car car : cars){
+            if(car.getMarka().equalsIgnoreCase(brend)){
+                result[index++] = car;
+            } 
+        }
+        
+        return result;
     }
 }
