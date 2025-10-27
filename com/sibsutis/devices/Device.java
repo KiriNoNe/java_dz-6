0868db1a02d15b6addae1be12797882f73d18b3e
Новиков Кирилл Васильевич ИП-512 <kirill.novikov.2005.nkv@gmail.com>
2025-10-28 00:53:07 +0700

    Реализован абстрактный класс Device

---
+++ com/sibsutis/devices/Device.java
@@ -0,0 +1,31 @@
+package com.sibsutis.devices;
+import com.sibsutis.Printable;
+abstract class Device implements Printable{
+    private int id;
+    private int price;
+    private String ip;
+
+    public Device(int id,int price, String ip){
+        this.id = id;
+        this.price = price;
+        this.ip = ip;
+    }
+    public int getId(){
+        return id;
+    }   
+    public int getPrice(){
+        return price;
+    }
+
+    public String getIp(){
+        return ip;
+    }
+    
+    @Override
+    public String print(){
+        System.out.println(this.id + this.price + this.ip);
+        return this.id + this.price + this.ip;
+    }
+
+    public abstract String getDeviceType();
+}
