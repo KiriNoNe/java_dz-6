6ea853cdfc3fa7b0effb232463c00ce965715555
Новиков Кирилл Васильевич ИП-512 <kirill.novikov.2005.nkv@gmail.com>
2025-10-28 01:40:26 +0700

    Методы equals и hashCode переопределены в классах потомках

--- com/sibsutis/devices/PersonalComputer.java
+++ com/sibsutis/devices/PersonalComputer.java
@@ -1,16 +1,29 @@
 package com.sibsutis.devices;
 import com.sibsutis.devices.Device;
 
-class PersonalComputer extends Device {
+public class PersonalComputer extends Device {
     public PersonalComputer(int id, int price){
         super(id, price, "");
     }
     public PersonalComputer(int id, int price, String ip){
         super(id, price, ip);
     }
-
     @Override
     public  String getDeviceType(){
         return "PersonalComputer";
+    }
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (!(obj instanceof PersonalComputer)) return false;
+        PersonalComputer pc = (PersonalComputer) obj;
+        return this.getId() == pc.getId() && this.getPrice() == pc.getPrice() && this.getIp() == pc.getIp();  
+    }
+    @Override
+    public int hashCode(){
+        int result =  Integer.hashCode(getId());
+        result = 31 * result + Intager.hashCode(getPrice());
+        result = 31* result + getIp().hashCode();
+        return result;
     }
 }
--- com/sibsutis/devices/Phone.java
+++ com/sibsutis/devices/Phone.java
@@ -12,4 +12,18 @@ public class Phone extends Device{
     public String getDeviceType(){
         return "Phone";
     }
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (!(obj instanceof Phone)) return false;
+        Phone ph = (Phone) obj;
+        return this.getId() == ph.getId() && this.getPrice() == ph.getPrice() && this.getIp() == ph.getIp();  
+    }
+    @Override
+    public int hashCode(){
+        int result =  Integer.hashCode(getId());
+        result = 31 * result + Intager.hashCode(getPrice());
+        result = 31* result + getIp().hashCode();
+        return result;
+    }
 }
