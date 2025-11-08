944bafe3d16076c196b001cfa3f21f363ac5fa3e
Новиков Кирилл Васильевич ИП-512 <kirill.novikov.2005.nkv@gmail.com>
2025-11-08 17:38:52 +0700

    3.

--- Cache.java
+++ Cache.java
@@ -1,7 +1,15 @@
+import java.util.LinkedList;
 public class Cache<T> {
     private int n;
+    private LinkedList<T> list = new LinkedList<>();
     
     public Cache(int n){
         this.n = n;
+    }
+    public void add(T item){
+        if ((list.size()+1) > n){
+            list.removeFirst();
+        } 
+        list.addLast(item);
     }
 }
